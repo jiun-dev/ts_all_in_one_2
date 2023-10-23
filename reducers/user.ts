79f19637822f8d7d40e43bf803a65e16f28d5f9a
‘jiun-dev’ <jiun.dev@gmail.com>
2023-10-23 15:11:20 +0900

    sec4 : reducer

---
+++ reducers/user.ts
@@ -0,0 +1,39 @@
+import { Reducer } from "redux";
+import {
+  LogInSuccessAction,
+  LogInSuccessData,
+  LogoutAction,
+} from "../actions/user";
+
+interface InitailState {
+  isLoggingIn: boolean;
+  data: LogInSuccessData | null;
+}
+
+const initialState = {
+  isLoggingIn: false,
+  data: null,
+};
+
+const userReducer: Reducer<InitailState, LogInSuccessAction | LogoutAction> = (
+  prevState = initialState,
+  action
+) => {
+  // 새로운 state 만들어주기
+  switch (action.type) {
+    case "LOG_IN_SUCCESS":
+      return {
+        ...prevState,
+        data: action.data,
+      };
+    case "LOG_OUT":
+      return {
+        ...prevState,
+        data: null,
+      };
+    default:
+      return prevState;
+  }
+};
+
+export default userReducer;
---
+++ section_4.md
@@ -0,0 +1,13 @@
+# redux
+
+- named export 방식
+- 선언과 사용의 이름이 같아야한다.
+- initialState에 상태의 기본값이 데이터로 저장되어있다.
+
+## redux는 상태를 변경해주는 역할
+
+- 상태가 존재하고 그 상태를 변경하는 action을 호출(dispatch)해야한다.
+
+## reducer는 상태를 바꾸는 규칙
+
+- 함수 매개변수state, action
